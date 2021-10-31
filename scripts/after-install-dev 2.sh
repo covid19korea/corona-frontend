1de908628159118864101dc4c477c70e545429bf
audgnssweet <audgnssweet@naver.com>
2021-10-31 17:45:18 +0900

    재배포

--- scripts/after-install-dev 2.sh
+++
@@ -1,4 +0,0 @@
-cd /home/ubuntu/corona-frontend-dev
-sudo chown -R ubuntu:ubuntu /home/ubuntu/corona-frontend-dev
-chmod 777 /home/ubuntu/corona-frontend-dev
-chmod 777 /home/ubuntu/corona-frontend-dev/*/**
--- scripts/after-install-prd 2.sh
+++
@@ -1,4 +0,0 @@
-cd /home/ubuntu/corona-frontend-prd
-sudo chown -R ubuntu:ubuntu /home/ubuntu/corona-frontend-prd
-chmod 777 /home/ubuntu/corona-frontend-prd
-chmod 777 /home/ubuntu/corona-frontend-prd/*/**
